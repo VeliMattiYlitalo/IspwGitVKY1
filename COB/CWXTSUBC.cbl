ff94e248b8840b32f5b5c17504929d86568da517
cwfi-vylitalo <cwfi-vylitalo@fi-106630n72.emea.cpwr.corp>
2020-11-11 09:59:07 +0200

    Mika3

--- COB/CWXTSUBC.cbl
+++ COB/CWXTSUBC.cbl
@@ -6,7 +6,7 @@
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
       *  XPEDITER/TSO TRAINING SESSIONS.                               *
-      *  MIKA2                                                          *
+      *  MIKA3                                                          *
       ******************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
