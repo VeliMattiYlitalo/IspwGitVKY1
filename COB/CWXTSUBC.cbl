48fe79de0039a70b87304891cd634a40e6158819
cwfi-vylitalo <cwfi-vylitalo@fi-106630n72.emea.cpwr.corp>
2020-11-11 13:59:08 +0200

    Back to original wo build

--- COB/CWXTSUBC.cbl
+++ COB/CWXTSUBC.cbl
@@ -6,7 +6,7 @@
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
       *  XPEDITER/TSO TRAINING SESSIONS.                               *
-      *  MIKA5                                                          *
+      *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
        DATA DIVISION.
